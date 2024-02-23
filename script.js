d1f55cb2adc6f41dacadc61368d4e3af711feeb7
Ostiminum <113224529+Ostiminum@users.noreply.github.com>
2024-02-23 10:10:32 +0800

    I-import ang web font bersyon ng JetBrains Mono kaysa yung galing sa system, dagdagan ng welcome box, dagdagan ng bangong command "reimu".

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 const FILES = [
-    "liham.jpg"
+    "liham.jpg",
+    "reimu.gif"
 ];
 
 const COMMANDS = [
-    ["buksan", "bubukas ng isang file na nagkaugnay sa binigay na file name (lalabas ng ERROR kapag walang nakitang file na nagkaugnay sa binigay na file name)."],
+    ["buksan <filename>", "bubukas ng isang file na nagkaugnay sa binigay na file name (lalabas ng ERROR kapag walang nakitang file na nagkaugnay sa binigay na file name)."],
     ["dir", "mag-display ng mga pangalan ng mga files at folder na nakalagay sa kasalukayang folder."],
     ["linis", "tatangalin ang lahat na mga text na naka-display sa console."],
-    ["tulong", "mag-display ng mga commands na sinusuporta ng system."]
+    ["tulong", "mag-display ng mga commands na sinusuporta ng system."],
+    ["reimu", "reimu"]
 ];
 
 let created_boxes = 0;
@@ -19,28 +21,34 @@ window.addEventListener("keydown", (event) => {
 
         created_boxes += 1;
 
-        let new_terminalBox = document.createElement("div");
-        new_terminalBox.classList.add("box");
-        new_terminalBox.classList.add("terminal-textbox");
-
-        let indicator = document.createElement("p");
-        indicator.appendChild(document.createTextNode("bahay> "))
-
-        let text_field = document.createElement("input");
-        text_field.classList.add("terminal-text");
-        text_field.type = "text";
-        text_field.id = "terminal-text" + created_boxes;
-
-        new_terminalBox.appendChild(indicator);
-        new_terminalBox.appendChild(text_field);
-
-        document.body.appendChild(new_terminalBox);
-        document.getElementById("terminal-text" + created_boxes).focus();
+        create_terminal_box();
     }
 })
 
 function os_setup() {
-    console.log("... Loading ...");
+    let welcome_box = document.createElement("div");
+    welcome_box.classList.add("box");
+    welcome_box.classList.add("welcome-box");
+
+    let welcome_text1 = document.createElement("p");
+    welcome_text1.appendChild(document.createTextNode(
+        "ReimuOS [Bersyon 1.0]."
+    ));
+
+    let welcome_text2 = document.createElement("p");
+    welcome_text2.appendChild(document.createTextNode(
+        "I-type ang 'tulong' para sa listahan ng mga commands na sinusuporta ng system."
+    ));
+    
+    welcome_box.appendChild(welcome_text1);
+    welcome_box.appendChild(document.createElement("br"));
+    welcome_box.appendChild(welcome_text2);    
+    welcome_box.appendChild(document.createElement("br"));
+    welcome_box.appendChild(document.createElement("p"));
+
+    document.body.appendChild(welcome_box);
+
+    create_terminal_box();
 }
 
 function process_command() {
@@ -115,8 +123,22 @@ function process_command() {
         }
 
         document.body.appendChild(new_helpBox);
+    } else if (current_text == "reimu") {
+        valid_input = true;
 
-    } 
+        let reimu_box = document.createElement("div");
+        reimu_box.classList.add("box");
+        reimu_box.classList.add("reimu-box");
+
+        let reimu_gif = document.createElement("img");
+        reimu_gif_path = document.createAttribute("src");
+        reimu_gif_path.value = "reimu.gif";
+
+        reimu_gif.setAttributeNode(reimu_gif_path);
+
+        reimu_box.appendChild(reimu_gif);
+        document.body.appendChild(reimu_box);
+    }
 
     if (!valid_input) {
         // displays an ERROR box
@@ -143,4 +165,24 @@ function create_para(text) {
     );
 
     return new_para;
+}
+
+function create_terminal_box() {
+    let new_terminalBox = document.createElement("div");
+    new_terminalBox.classList.add("box");
+    new_terminalBox.classList.add("terminal-textbox");
+
+    let indicator = document.createElement("p");
+    indicator.appendChild(document.createTextNode("tahanan> "))
+
+    let text_field = document.createElement("input");
+    text_field.classList.add("terminal-text");
+    text_field.type = "text";
+    text_field.id = "terminal-text" + created_boxes;
+
+    new_terminalBox.appendChild(indicator);
+    new_terminalBox.appendChild(text_field);
+
+    document.body.appendChild(new_terminalBox);
+    document.getElementById("terminal-text" + created_boxes).focus();
 }
